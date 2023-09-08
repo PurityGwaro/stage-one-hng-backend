c1cac8c60ffb49bb49b014762adfe59e05ce9b3d
PurityGwaro <puritygwaro@gmail.com>
2023-09-08 14:14:29 +0300

    initial commit

---
+++ data/profile.js
@@ -0,0 +1,8 @@
+const profileData = [
+    {
+        slack_name: "Purity Gwaro",
+        track: "Frontend | Backend",
+        github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
+        github_repo_url: "https://github.com/username/repo",
+    },
+]
