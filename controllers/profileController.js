6df64d6d3a50d69b56de5af33e1e8acbdbdaa428
PurityGwaro <puritygwaro@gmail.com>
2023-09-09 01:08:56 +0300

    response object

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,5 +1,43 @@
+const fs = require("fs");
+
 const getProfile = async (req, res) => {
-  res.status(200).json({ message: "getting profile" });
+  const { slack_name, track } = req.query;
+
+  if (!slack_name || !track) {
+    return res.status(400).json({
+      error:
+        "You might be missing parameters: slack_name and track are required in your query string",
+    });
+  }
+
+  fs.readFile("./data/profile.js", "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Error fetching the profile" });
+    }
+    const profileCollection = JSON.parse(data);
+
+    console.log(profileCollection[0].slack_name);
+
+    // use slack_name and track to find profile
+    const lowercaseSlackName = slack_name.toLowerCase();
+    const lowercaseTrack = track.toLowerCase();
+
+    // Use lowercase for comparison
+    const findProfile = profileCollection.find(
+      (profile) =>
+        profile.slack_name.toLowerCase() === lowercaseSlackName &&
+        Array.isArray(profile.track) &&
+        profile.track.some(
+          (trackItem) => trackItem.toLowerCase() === lowercaseTrack
+        )
+    );
+
+    if (findProfile) {
+      res.status(200).json({ ...findProfile, status_code: 200 });
+    } else {
+      res.status(404).json({ error: "Profile not found. If it's two names write it this way: purity gwaro OR Purity Gwaro. The space has to be there.",status_code: 404 });
+    }
+  });
 };
 
 module.exports = {
--- data/profile.js
+++ data/profile.js
@@ -1,8 +1,14 @@
-const profileData = [
-    {
-        slack_name: "Purity Gwaro",
-        track: "Frontend | Backend",
-        github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
-        github_repo_url: "https://github.com/username/repo",
-    },
+[
+  {
+    "slack_name": "Purity Gwaro",
+    "track": ["frontend","backend"],
+    "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+    "github_repo_url": "https://github.com/username/repo"
+  },
+  {
+    "slack_name": "John",
+    "track": ["frontend","backend"],
+    "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+    "github_repo_url": "https://github.com/username/repo"
+  }
 ]
