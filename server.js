3ba2f8f7471083a5c01a2fed340ae133a61361c9
PurityGwaro <puritygwaro@gmail.com>
2023-09-09 01:44:24 +0300

    add instructions

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.json());
 
 app.use("/api", require("./routes/profile"));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/instructions.html');
+});
+
 app.listen(PORT, (err) => {
   if (err) console.log(err);
   console.log(`Server listening on PORT ${PORT}`);
